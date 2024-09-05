90448e522a8fb340583925fb0180600626c88219
JoeMBelli <jmb225@sunyorange.edu>
2024-09-05 19:02:48 -0400

    Update temp_converter.py
    new changes

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python3
 
-# process
+#!/usr/bin/python3
 def c2f(c):
     return c * 9 / 5 + 32
-
 def main(cel):
     return c2f(cel)
-
 if __name__ == "__main__":
     cel = 100         # input
     print(main(cel))  # output
